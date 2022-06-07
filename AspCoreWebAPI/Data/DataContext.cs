6166084a38a73bc99e1f8ef4c07ef55dc40dd206
Krishna Mishra <kuleen24@gmail.com>
2022-06-07 22:15:06 +0530

    Removed unused

---
+++ AspCoreWebAPI/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using AspCoreWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreWebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+         
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+
+}
